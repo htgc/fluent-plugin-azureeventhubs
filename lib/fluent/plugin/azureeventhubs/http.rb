6c74414a81734a045bbe9f1794311c468ff121e5
root <root@wordpress.(none)>
2014-11-16 21:37:01 +0900

    Initial commit

---
+++ lib/fluent/plugin/azureeventhubs/http.rb
@@ -0,0 +1,57 @@
+
+class AzureEventHubsHttpSender
+  def initialize(connection_string, hub_name)
+    require 'openssl'
+    require 'base64'
+    require 'net/http'
+    require 'json'
+    require 'cgi'
+    require 'time'
+    @connection_string = connection_string
+    @hub_name = hub_name
+    @expires_in_mins = 3600
+
+    if @connection_string.count(';') != 2
+      raise "Connection String format is not correct"
+    end
+
+    @connection_string.split(';').each do |part|
+      if ( part.index('Endpoint') == 0 )
+        @endpoint = 'https' + part[11..-1]
+      elsif ( part.index('SharedAccessKeyName') == 0 )
+        @sas_key_name = part[20..-1]
+      elsif ( part.index('SharedAccessKey') == 0 )
+        @sas_key_value = part[16..-1]
+      end
+    end
+    @uri = URI.parse("#{@endpoint}#{@hub_name}/messages")
+  end
+
+  def generate_sas_token(uri)
+    target_uri = CGI.escape(uri.downcase).downcase
+    expires = Time.now.to_i + @expires_in_mins
+    to_sign = "#{target_uri}\n#{expires}";
+    signature = CGI.escape(Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), @sas_key_value, to_sign)).strip())
+
+    token = "SharedAccessSignature sr=#{target_uri}&sig=#{signature}&se=#{expires}&skn=#{@sas_key_name}"
+    p token
+    return token
+  end
+
+  private :generate_sas_token
+
+  def send(payload)
+    token = generate_sas_token(@uri.to_s)
+    headers = {
+      'Content-Type' => 'application/atom+xml;type=entry;charset=utf-8',
+      'Authorization' => token
+    }
+    https = Net::HTTP.new(@uri.host, @uri.port)
+    https.use_ssl = true
+    #req = Net::HTTP::Post.new("#{@hub_name}/messages", initheader = {'Content-Type' => 'application/atom+xml;type=entry;charset=utf-8', 'Authorization' => token})
+    req = Net::HTTP::Post.new(@uri.request_uri, headers)
+    req.body = payload.to_json
+    res = https.request(req)
+    p res.to_s
+  end
+end
---
+++ lib/fluent/plugin/out_azureeventhubs_buffered.rb
@@ -0,0 +1,43 @@
+#module Fluent
+
+  class AzureEventHubsOutputBuffered < Fluent::BufferedOutput
+    Fluent::Plugin.register_output('azureeventhubs_buffered', self)
+
+    config_param :connection_string, :string
+    config_param :hub_name, :string
+    config_param :include_tag, :bool, :default => false
+    config_param :include_time, :bool, :default => false
+    config_param :tag_time_name, :string, :default => 'time'
+    config_param :expires_mins, :integer, :default => 3600 # 60min
+    config_param :type, :string, :default => 'https' # https / amqps (Not Implemented) 
+
+    def configure(conf)
+      super
+      case @type
+      when 'amqps'
+        raise NotImplementedError
+      else
+        require_relative 'azureeventhubs/http'
+        @sender = AzureEventHubsHttpSender.new(@connection_string, @hub_name)
+      end
+    end
+
+    def format(tag, time, record)
+      [tag, time, record].to_msgpack
+    end
+
+    def write(chunk)
+      chunk.msgpack_each { |tag, time, record|
+        p record.to_s
+        if @include_tag
+          record['tag'] = tag
+        end
+        if @include_time
+          record[@tag_time_name] = time
+        end
+        @sender.send(record)
+      }
+    end
+  end
+#end
+
