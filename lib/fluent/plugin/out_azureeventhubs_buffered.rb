77eddf83e5b53f85fc1991d5789941b96222c45e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2017-05-16 12:10:43 +0900

    Use Fluentd v0.14 API

--- lib/fluent/plugin/out_azureeventhubs_buffered.rb
+++ lib/fluent/plugin/out_azureeventhubs_buffered.rb
@@ -1,7 +1,11 @@
-#module Fluent
+module Fluent::Plugin
 
-  class AzureEventHubsOutputBuffered < Fluent::BufferedOutput
+  class AzureEventHubsOutputBuffered < Output
     Fluent::Plugin.register_output('azureeventhubs_buffered', self)
+
+    helpers :compat_parameters
+
+    DEFAULT_BUFFER_TYPE = "memory"
 
     config_param :connection_string, :string
     config_param :hub_name, :string
@@ -9,14 +13,20 @@
     config_param :include_time, :bool, :default => false
     config_param :tag_time_name, :string, :default => 'time'
     config_param :expiry_interval, :integer, :default => 3600 # 60min
-    config_param :type, :string, :default => 'https' # https / amqps (Not Implemented) 
+    config_param :type, :string, :default => 'https' # https / amqps (Not Implemented)
     config_param :proxy_addr, :string, :default => ''
     config_param :proxy_port, :integer,:default => 3128
     config_param :open_timeout, :integer,:default => 60
     config_param :read_timeout, :integer,:default => 60
     config_param :message_properties, :hash, :default => nil
 
+    config_section :buffer do
+      config_set_default :@type, DEFAULT_BUFFER_TYPE
+      config_set_default :chunk_keys, ['tag']
+    end
+
     def configure(conf)
+      compat_parameters_convert(conf, :buffer)
       super
       case @type
       when 'amqps'
@@ -44,5 +54,4 @@
       }
     end
   end
-#end
-
+end
